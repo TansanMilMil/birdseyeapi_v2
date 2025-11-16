6affa6d5d68bc785add6a4e7bcc754e84116c168
TansanMilMil <tmdev2929@gmail.com>
2025-11-16 12:18:26 +0900

    feat: refactor deploy script

--- deploy.sh
+++ deploy.sh
@@ -34,21 +34,27 @@ docker compose exec go ./test.sh
 
 echo "build ----------------------------"
 docker compose exec go ./build.sh
-tar cvfz ./go/dist.tgz ./go/dist/
 
 echo "docker compose teardown ----------------------------"
 docker compose down
 
-echo "scp files to venus ----------------------------"
-ssh $VENUS_SSH_HOST mkdir -p $VENUS_TARGET_DIR/go
-scp ./go/dist.tgz $VENUS_SSH_HOST:$VENUS_TARGET_DIR/go/dist.tgz
-ssh $VENUS_SSH_HOST tar xvfz $VENUS_TARGET_DIR/go/dist.tgz -C $VENUS_TARGET_DIR
+echo "create deploy package ----------------------------"
+rm -rf ./deploy-temp
+mkdir -p ./deploy-temp
+cp -r ./go/dist ./deploy-temp/
+cp ./docker-compose.yml ./deploy-temp/
+cp ./go-entrypoint.sh ./deploy-temp/
+cp ./init_db.sh ./deploy-temp/
+cp -r ./nginx ./deploy-temp/
+cp -r ./mysql ./deploy-temp/
+tar czf ./deploy.tgz -C ./deploy-temp .
+rm -rf ./deploy-temp
 
-scp ./docker-compose.yml $VENUS_SSH_HOST:$VENUS_TARGET_DIR/docker-compose.yml
-scp ./go-entrypoint.sh $VENUS_SSH_HOST:$VENUS_TARGET_DIR/go-entrypoint.sh
-scp ./init_db.sh $VENUS_SSH_HOST:$VENUS_TARGET_DIR/init_db.sh
-scp -r ./nginx $VENUS_SSH_HOST:$VENUS_TARGET_DIR/
-scp -r ./mysql $VENUS_SSH_HOST:$VENUS_TARGET_DIR/
+echo "scp files to venus ----------------------------"
+ssh $VENUS_SSH_HOST mkdir -p $VENUS_TARGET_DIR
+scp ./deploy.tgz $VENUS_SSH_HOST:$VENUS_TARGET_DIR/deploy.tgz
+ssh $VENUS_SSH_HOST "tar xzf $VENUS_TARGET_DIR/deploy.tgz -C $VENUS_TARGET_DIR && rm $VENUS_TARGET_DIR/deploy.tgz"
+rm ./deploy.tgz
 echo ""
 echo "current files in $VENUS_SSH_HOST:$VENUS_TARGET_DIR"
 ssh $VENUS_SSH_HOST ls -alh $VENUS_TARGET_DIR
