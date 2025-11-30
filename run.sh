87af25f6e28cad2763877f52edbc5986ff67491c
TansanMilMil <tmdev2929@gmail.com>
2025-11-30 14:10:00 +0000

    feat: add http_x_forwarded_for as label at Promtail

--- run.sh
+++ run.sh
@@ -4,20 +4,4 @@ cd `dirname $0`
 BASE_DIR="go"
 GO_FILE="$BASE_DIR/src/main.go"
 
-NO_DOCKER_COMPOSE=false
-
-for ARG in "$@"; do
-    case "$ARG" in
-        --no-docker-compose)
-            NO_DOCKER_COMPOSE=true
-            ;;
-    esac
-done
-
-if $NO_DOCKER_COMPOSE; then
-    COMMAND_PREFIX="No docker compose exec go..."
-    go run $GO_FILE
-else
-    docker compose up -d
-    docker compose exec go go run $GO_FILE
-fi
+go run $GO_FILE
